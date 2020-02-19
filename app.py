47a514f17f172c3bed52879376952dad77618e6e
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-02-19 21:28:41 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,71 @@
+from flask import Flask, jsonify, request, url_for
+from werkzeug.utils import secure_filename
+import os
+import cv2
+import numpy as np
+from PIL import Image
+
+app = Flask(__name__)
+
+UPLOAD_FOLDER = 'images'
+ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'}
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/')
+def hello_world():
+    return jsonify({
+        'message': 'You are connected to FLASK server'
+    })
+
+
+@app.route('/send', methods=['POST'])
+def debug_post():
+    return jsonify({
+        'message': 'received',
+        'data': request.json,
+    })
+
+
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+def process_image(image):
+    """
+    Processing the image
+    :param image:
+    :return:
+    """
+    np_image = np.array(Image.open(image))
+    rotated = cv2.rotate(np_image, cv2.ROTATE_90_CLOCKWISE)
+    return cv2.cvtColor(rotated, cv2.COLOR_BGR2RGB)
+
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            return jsonify({
+                'message': 'no file uploaded'
+            })
+        file = request.files['file']
+        # if user does not select file, browser also
+        # submit an empty part without filename
+        if file.filename == '':
+            return jsonify({
+                'message': 'No file uploaded'
+            })
+        if file and allowed_file(file.filename):
+            processed_image = process_image(file)
+            filename = secure_filename(file.filename)
+            cv2.imwrite(os.path.join(app.config['UPLOAD_FOLDER'], filename), processed_image)
+            return jsonify({
+                'message': 'Successfully uploaded',
+                'data': {
+                    'filename': url_for('upload_file', filename=filename),
+                },
+            })
