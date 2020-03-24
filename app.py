06a933f5e18bb98fb52b346934c9b378366e3ff7
jasonlimantoro <jasonlimantoro99@gmail.com>
2020-03-25 02:27:56 +0800

    [FIX]: preload the nn model - reformat code

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 from flask import Flask, jsonify, request, url_for, render_template
-from werkzeug.utils import secure_filename
-from modules.nn import recognizer
 import os
 import glob
+import logging
+from modules.nn import recognizer
+
+
+logging.basicConfig(format='%(asctime)s - %(message)s', level=logging.INFO)
 
 app = Flask(__name__, static_folder='static', static_url_path='/static')
 
@@ -14,26 +17,22 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/')
 def hello_world():
-    return jsonify({
-        'message': 'You are connected to FLASK server'
-    })
+    return jsonify({'message': 'You are connected to FLASK server'})
 
 
 @app.route('/send', methods=['POST'])
 def debug_post():
-    return jsonify({
-        'message': 'received',
-        'data': request.json,
-    })
+    return jsonify({'mesage': 'received', 'data': request.json})
 
 
 def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 def process_image(image):
-    return recognizer.recognize(image, target=f'{UPLOAD_FOLDER}/labelled', debug=f'{UPLOAD_FOLDER}/debug')
+    return recognizer.recognize(
+        image, target=f'{UPLOAD_FOLDER}/labelled', debug=f'{UPLOAD_FOLDER}/debug'
+    )
 
 
 @app.route('/upload', methods=['POST'])
@@ -41,32 +40,35 @@ def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
         if 'file' not in request.files:
-            return jsonify({
-                'message': 'no file uploaded'
-            })
+            return jsonify({'message': 'no file uploaded'})
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            return jsonify({
-                'message': 'No file uploaded. Perhaps you forgot to select the file'
-            })
+            return jsonify(
+                {'message': 'No file uploaded. Perhaps you forgot to select the file'}
+            )
         if file and allowed_file(file.filename):
+            filename = file.filename
+            logging.info(f'Processing image {filename}')
             detections = process_image(file)
-            filename = secure_filename(file.filename)
-            return jsonify({
-                'message': 'Successfully uploaded',
-                'data': {
-                    'filename': url_for('upload_file', filename=filename),
-                    'detections': detections,
-                },
-            })
+            logging.info(f'Processing image {filename} finished')
+            return jsonify(
+                {
+                    'message': 'Successfully uploaded',
+                    'data': {
+                        'filename': url_for('upload_file', filename=filename),
+                        'detections': detections,
+                    },
+                }
+            )
 
 
 @app.route('/home')
 def home():
-    images = glob.glob(f"{app.config['UPLOAD_FOLDER']}/labelled/*.jpg") +\
-             glob.glob(f"{app.config['UPLOAD_FOLDER']}/labelled/*.jpeg")
+    images = glob.glob(f"{app.config['UPLOAD_FOLDER']}/labelled/*.jpg") + glob.glob(
+        f"{app.config['UPLOAD_FOLDER']}/labelled/*.jpeg"
+    )
     images = [{"name": os.path.basename(i), "src": i} for i in images]
     images = sorted(images, key=lambda x: x['name'], reverse=True)
     return render_template('home.html', images=images)
--- modules/nn/recognizer.py
+++ modules/nn/recognizer.py
@@ -10,7 +10,7 @@ import os
 
 MAX_BOXES_TO_DRAW = 5
 THRESHOLD = 0.9
-AREA_THRESHOLD = 0.01
+AREA_THRESHOLD = 0.02
 
 
 def scale_down(image, percentage=60):
@@ -34,69 +34,64 @@ def determine_section(xmin, xmax):
         return 2
 
 
-def recognize(
-    image,
-    train_dir='modules/nn/training',
-    model_dir='modules/nn/inference_graph',
-    target='',
-    debug='',
-):
-    model_name = model_dir
-    # Path to frozen detection graph. This is the actual model that is used for the object detection.
-    path_to_frozen_graph = model_name + '/frozen_inference_graph.pb'
+model_name = 'modules/nn/inference_graph'
+train_dir = 'modules/nn/training'
+path_to_frozen_graph = model_name + '/frozen_inference_graph.pb'
+path_to_labels = f"{train_dir}/labelmap.pbtxt"
 
-    # List of the strings that is used to add correct label for each box.
-    path_to_labels = f"{train_dir}/labelmap.pbtxt"
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.gfile.GFile(path_to_frozen_graph, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+category_index = label_map_util.create_category_index_from_labelmap(
+    path_to_labels, use_display_name=True
+)
 
-    detection_graph = tf.Graph()
-    with detection_graph.as_default():
-        od_graph_def = tf.GraphDef()
-        with tf.gfile.GFile(path_to_frozen_graph, 'rb') as fid:
-            serialized_graph = fid.read()
-            od_graph_def.ParseFromString(serialized_graph)
-            tf.import_graph_def(od_graph_def, name='')
-    category_index = label_map_util.create_category_index_from_labelmap(
-        path_to_labels, use_display_name=True
+
+def run_inference_for_single_image(image, graph, tensor_dict, sess):
+    if 'detection_masks' in tensor_dict:
+        # The following processing is only for single image
+        detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
+        detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
+        # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
+        real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
+        detection_boxes = tf.slice(detection_boxes, [0, 0], [real_num_detection, -1])
+        detection_masks = tf.slice(
+            detection_masks, [0, 0, 0], [real_num_detection, -1, -1]
+        )
+        detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
+            detection_masks, detection_boxes, image.shape[0], image.shape[1]
+        )
+        detection_masks_reframed = tf.cast(
+            tf.greater(detection_masks_reframed, 0.5), tf.uint8
+        )
+        # Follow the convention by adding back the batch dimension
+        tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
+    image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+
+    # Run inference
+    output_dict = sess.run(
+        tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)}
     )
 
-    def run_inference_for_single_image(image, graph):
-        if 'detection_masks' in tensor_dict:
-            # The following processing is only for single image
-            detection_boxes = tf.squeeze(tensor_dict['detection_boxes'], [0])
-            detection_masks = tf.squeeze(tensor_dict['detection_masks'], [0])
-            # Reframe is required to translate mask from box coordinates to image coordinates and fit the image size.
-            real_num_detection = tf.cast(tensor_dict['num_detections'][0], tf.int32)
-            detection_boxes = tf.slice(
-                detection_boxes, [0, 0], [real_num_detection, -1]
-            )
-            detection_masks = tf.slice(
-                detection_masks, [0, 0, 0], [real_num_detection, -1, -1]
-            )
-            detection_masks_reframed = utils_ops.reframe_box_masks_to_image_masks(
-                detection_masks, detection_boxes, image.shape[0], image.shape[1]
-            )
-            detection_masks_reframed = tf.cast(
-                tf.greater(detection_masks_reframed, 0.5), tf.uint8
-            )
-            # Follow the convention by adding back the batch dimension
-            tensor_dict['detection_masks'] = tf.expand_dims(detection_masks_reframed, 0)
-        image_tensor = tf.get_default_graph().get_tensor_by_name('image_tensor:0')
+    # all outputs are float32 numpy arrays, so convert types as appropriate
+    output_dict['num_detections'] = int(output_dict['num_detections'][0])
+    output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(
+        np.uint8
+    )
+    output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
+    output_dict['detection_scores'] = output_dict['detection_scores'][0]
+    if 'detection_masks' in output_dict:
+        output_dict['detection_masks'] = output_dict['detection_masks'][0]
+    return output_dict
 
-        # Run inference
-        output_dict = sess.run(
-            tensor_dict, feed_dict={image_tensor: np.expand_dims(image, 0)}
-        )
 
-        # all outputs are float32 numpy arrays, so convert types as appropriate
-        output_dict['num_detections'] = int(output_dict['num_detections'][0])
-        output_dict['detection_classes'] = output_dict['detection_classes'][0].astype(
-            np.uint8
-        )
-        output_dict['detection_boxes'] = output_dict['detection_boxes'][0]
-        output_dict['detection_scores'] = output_dict['detection_scores'][0]
-        if 'detection_masks' in output_dict:
-            output_dict['detection_masks'] = output_dict['detection_masks'][0]
-        return output_dict
+def recognize(
+    image, target='', debug='',
+):
 
     with detection_graph.as_default():
         with tf.Session() as sess:
@@ -116,10 +111,11 @@ def recognize(
                         tensor_name
                     )
 
-            print(f"Processing {image.filename}...")
             image_np = np.array(Image.open(image))
             np.expand_dims(image_np, axis=0)
-            output_dict = run_inference_for_single_image(image_np, detection_graph)
+            output_dict = run_inference_for_single_image(
+                image_np, detection_graph, tensor_dict, sess,
+            )
             vis_util.visualize_boxes_and_labels_on_image_array(
                 image_np,
                 output_dict['detection_boxes'],
@@ -164,14 +160,11 @@ def recognize(
                             'section': section,
                         }
                     )
+            rgb_image = convert_to_rgba(image_np)
             if found:
-                cv2.imwrite(
-                    os.path.join(target, image.filename), convert_to_rgba(image_np)
-                )
-            if debug:
-                cv2.imwrite(
-                    os.path.join(debug, image.filename), convert_to_rgba(image_np)
-                )
+                cv2.imwrite(os.path.join(target, image.filename), rgb_image)
             else:
                 print('Not found any known image', image.filename)
+            if debug:
+                cv2.imwrite(os.path.join(debug, image.filename), rgb_image)
             return detections
